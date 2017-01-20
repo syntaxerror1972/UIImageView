8b568f980c5868d9000943a456a6dfed3b78129f
Shrawan Shinde <shrawanshinde@Shrawans-MacBook-Pro.local>
2017-01-21 00:25:31 +0530

    A UIImageView object displays a single image or a sequence of animated images in your interface. Image views let you efficiently draw any image that can be specified using a UIImage object. For example, you can use this class to display the contents of many standard image files, such as JPEG and PNG files. You can configure image views programmatically or in your storyboard file and change the images they display at runtime. For animated images, you can also use the methods of this class to start and stop the animation and specify other animation parameters.

--- UIImageView/ViewController.swift
+++ UIImageView/ViewController.swift
@@ -9,10 +9,30 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    var roundImage = UIImageView()
+    var simpleImage = UIImageView()
+
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        //simple image
+        simpleImage = UIImageView(frame:CGRect(x: 50, y: 50, width: 200, height: 200));
+        simpleImage.image = UIImage(named:"demo.jpg")
+        
+        //round image
+        roundImage = UIImageView(frame:CGRect(x: 50, y: 260, width: 200, height: 200));
+        roundImage.image = UIImage(named:"demo.jpg")
+        roundImage.layer.borderWidth = 1
+        roundImage.layer.masksToBounds = false
+        roundImage.layer.borderColor = UIColor.black.cgColor
+        roundImage.layer.cornerRadius = roundImage.frame.height/2
+        roundImage.clipsToBounds = true
+        
+        self.view.addSubview(simpleImage)
+        self.view.addSubview(roundImage)
     }
 
     override func didReceiveMemoryWarning() {
